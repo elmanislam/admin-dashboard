ba9b5eac46f5839781c6e705f49bebe934b60544
Elman Islam <eislam@uoguelph.ca>
2023-07-18 11:41:44 -0400

    Create layout

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+/*let sidebarSwitch = false;
+
+function toggleSidebar() {
+
+  if (sidebarSwitch == true) {
+    $("body").css("  grid-template-columns", "0 1fr");
+    $( ".sidebar" ).css( "display", "none" );
+    $(".header").css("grid-column", "1 / 3");
+    sidebarSwitch = false;
+  }
+
+  else {
+    $("body").css("  grid-template-columns", "clamp(200px, 15%, 500px) 3fr");
+    $( ".sidebar" ).css( "display", "" );
+    $(".header").css("grid-column", "1 / 3");
+    sidebarSwitch = true;
+
+  }
+}; */
